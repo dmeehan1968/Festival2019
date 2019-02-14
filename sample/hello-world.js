6c2b063c1fcdefba9a59fb5b2a560aee53336eaa
Dave Meehan <dave_meehan@replicated.co.uk>
2019-02-14 11:27:33 +0000

    Move hello world handler to own module

---
+++ sample/hello-world.js
@@ -0,0 +1,5 @@
+function helloWorld(res, req) {
+  res.send('Hello World')
+}
+
+module.exports = helloWorld
--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const helloWorld = require('../sample/hello-world')
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', helloWorld)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
