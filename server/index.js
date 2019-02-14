87a9f4a51b2ed026787a44537f7872439bdbcbbe
Dave Meehan <dave_meehan@replicated.co.uk>
2019-02-14 11:45:45 +0000

    use require directly to avoid var decl

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const helloWorld = require('../sample/hello-world')
 
-app.get('/', helloWorld)
+app.get('/', require('../sample/hello-world'))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
