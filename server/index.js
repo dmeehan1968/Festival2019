6a1fb0b8665c7aa5cbe590524badbbbee535a294
Dave Meehan <dave_meehan@replicated.co.uk>
2019-02-14 12:25:21 +0000

    Promisify startup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,31 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', require('../sample/hello-world'))
+async function boot(context) {
+  context.app = express()
+  context.app.set('db', {})
+  return context
+}
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+async function routes(context) {
+  context.app.get('/', require('../sample/hello-world'))
+  return context
+}
+
+function listen(context) {
+  return new Promise(resolve => {
+    context.app.listen(context.port, (server) => {
+      context.server = server
+      resolve(context)
+    })
+  })
+}
+
+function log(context) {
+  console.log(`Listening on port ${context.port}`)
+}
+
+boot({ port: 3000 })
+  .then(routes)
+  .then(listen)
+  .then(log)
