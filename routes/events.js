db20cb1f931ad7e6698a28260cb85d152560f324
Dave Meehan <dave_meehan@replicated.co.uk>
2019-02-14 16:46:09 +0000

    Add event associations and refactor startup

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,18 @@
-module.exports = (req, res) => {
+module.exports = (req, res, next) => {
   const db = req.app.get('db')
   const Events = db.models.events
   return new Promise(resolve => {
-    Events.findAll({ include: [ 'contact', 'bookingcontact', 'venue', 'preferred_image' ] }).then(events => {
-      res.json(events)
+    Events
+      .findAll({
+        include: [ 'contact', 'bookingcontact', {
+          association: 'venue',
+          include: [ 'venuecontact', 'addresscontact', 'disabled', 'toilet', 'dog' ]
+        }, 'preferred_image' ],
+        order: [ 'title' ]
+      })
+      .then(events => {
+        res.json({ success: true, events })
+      })
+      .catch(next)
     })
-  })
 }
--- server/index.js
+++ server/index.js
@@ -4,33 +4,72 @@ const Sequelize = require('sequelize')
 const fs = require('fs')
 const path = require('path')
 
-async function boot(context) {
-  context.app = express()
-  const db = new Sequelize('10p_festival2017', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-    operatorsAliases: false,
-  })
-  const models = path.join(__dirname, '../models')
-  fs.readdirSync(models).forEach(filename => {
-    if (!/_relatesto_/.test(filename)) {
-      const matchExtension = /\.[^\\.]+$/
-      filename = filename.replace(matchExtension, '')
-      db.import(path.join(models, filename))
-    }
-  })
+function associateSchema(db) {
   db.models.events.belongsTo(db.models.contacts, { as: 'contact', foreignKey: 'contact_id' })
   db.models.events.belongsTo(db.models.contacts, { as: 'bookingcontact', foreignKey: 'bookingcontact_id' })
   db.models.events.belongsTo(db.models.venues, { as: 'venue', foreignKey: 'venue_id' })
   db.models.events.belongsTo(db.models.images, { as: 'preferred_image', foreignKey: 'preferred_image_id' })
+  db.models.venues.belongsTo(db.models.contacts, { foreignKey: 'venuecontact_id', as: 'venuecontact' })
+  db.models.venues.belongsTo(db.models.contacts, { foreignKey: 'addresscontact_id', as: 'addresscontact' })
+  db.models.venues.belongsTo(db.models.tags, { foreignKey: 'disabled_id', as: 'disabled' })
+  db.models.venues.belongsTo(db.models.tags, { foreignKey: 'toilet_id', as: 'toilet' })
+  db.models.venues.belongsTo(db.models.tags, { foreignKey: 'dog_id', as: 'dog' })
   db.sync({ alter: false })
-  context.app.set('db', db)
-  return context
+}
+
+function stripExtension(file) {
+  const matchExtension = /\.[^\\.]+$/
+  return file.replace(matchExtension, '')
+}
+
+function excludeRelations(file) {
+  return !/_relatesto_/.test(file)
+}
+
+function indexModels(dirname) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(dirname, (err, files) => {
+      if (err) return reject(err);
+      resolve(files
+        .map(file => path.join(dirname, file))
+        .map(stripExtension)
+        .filter(excludeRelations)
+      )
+    })
+  })
+}
+
+async function importSchema(db, models) {
+  models.forEach(model => db.import(model))
+}
+
+async function database(context) {
+  const db = new Sequelize(context.dbschema, context.dbuser, context.dbpass, {
+    host: context.dbhost,
+    dialect: 'mysql',
+    operatorsAliases: false,
+  })
+
+  return indexModels(path.join(__dirname, '../models'))
+    .then(importSchema.bind(null, db))
+    .then(associateSchema.bind(null, db))
+    .then(() => db)
+}
+
+async function boot(context) {
+  context.app = express()
+  return database(context)
+    .then(db => context.app.set('db', db))
+    .then(() => context)
 }
 
 async function routes(context) {
   context.app.get('/', require('../sample/hello-world'))
   context.app.get('/events', require('../routes/events'))
+  context.app.use((err, req, res, next) => {
+    console.error(err.message)
+    res.json({ success: false, error: err.message })
+  })
   return context
 }
 
@@ -47,7 +86,7 @@ function log(context) {
   console.log(`Listening on port ${context.port}`)
 }
 
-boot({ port: 3000 })
+boot({ port: 3000, dbhost: 'localhost', dbschema: '10p_festival2017', dbuser: 'root', dbpass: 'root' })
   .then(routes)
   .then(listen)
   .then(log)
