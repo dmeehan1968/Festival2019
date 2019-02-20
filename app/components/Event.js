10f96c15753fc3c4e1616dd12628e3215596f270
Dave Meehan <dave_meehan@replicated.co.uk>
2019-02-20 16:37:21 +0000

    Change source order of event children

--- app/components/Event.js
+++ app/components/Event.js
@@ -3,9 +3,6 @@ import React from 'react'
 const Event = ({ title, subtitle, shortdesc, preferred_image }) => {
   return (
     <article className="event">
-      {preferred_image && preferred_image.filename ? <section className="image">
-        <img src={preferred_image.filename} alt={preferred_image.title} />
-      </section> : null}
       <section className="meta">
         <h1 className="title">
           <a href="#">{title}</a>
@@ -13,6 +10,9 @@ const Event = ({ title, subtitle, shortdesc, preferred_image }) => {
         {subtitle ? <h1 className="subtitle">{subtitle}</h1> : null}
         <p className="shortdesc">{shortdesc}</p>
       </section>
+      {preferred_image && preferred_image.filename ? <section className="image">
+        <img src={preferred_image.filename} alt={preferred_image.title} />
+      </section> : null}
     </article>
   )
 }
