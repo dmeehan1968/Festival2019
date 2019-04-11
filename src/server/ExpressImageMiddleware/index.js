56013d59d7187b3984f123ab703ae900d14f0f26
Dave Meehan <dave_meehan@replicated.co.uk>
2019-04-11 19:49:42 +0100

    Add cache control headers

--- src/server/ExpressImageMiddleware/index.js
+++ src/server/ExpressImageMiddleware/index.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import fs from 'fs'
 import os from 'os'
+import moment from 'moment'
 import imageResizeAndCache from './imageResizeAndCache'
 
 export default (
@@ -36,7 +37,29 @@ export default (
 
     imageResizeAndCache(src, query, options)
     .then(cachedFile => {
-      // res.type(path.extname(cachedFile))
+      const { name: etag } = path.parse(cachedFile)
+      const stat = fs.statSync(cachedFile)
+
+      res.set('ETag', etag)
+      res.set('Last-Modified', stat.mtime.toUTCString())
+      res.set('Cache-Control', 'no-cache, max-age=0')
+      res.type(path.extname(cachedFile))
+
+      const modifiedSince = req.get('If-Modified-Since')
+
+      if (modifiedSince) {
+        const age = moment(modifiedSince).diff(moment(stat.mtime), 'seconds')
+        if (age >= 0) {
+          return res.status(304).end()
+        }
+      }
+
+      const noneMatch = req.get('If-None-Match')
+
+      if (noneMatch && noneMatch !== etag) {
+        return res.status(304).end()
+      }
+
       fs.createReadStream(cachedFile).pipe(res)
     })
 
