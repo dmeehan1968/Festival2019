1f92ab542aabbbfb9d1e42053eb39ef6267085fd
Dave Meehan <dave_meehan@replicated.co.uk>
2019-04-11 18:51:22 +0100

    Extract ImageResizeAndCache

---
+++ src/server/ExpressImageMiddleware/imageResizeAndCache.js
@@ -0,0 +1,70 @@
+import path from 'path'
+import fs from 'fs'
+import moment from 'moment'
+import sharp from 'sharp'
+import hash from 'object-hash'
+import shrinkToFit from './shrinkToFit'
+
+export default (src, query, options) => {
+
+  let diff={}
+  let transform = sharp(src)
+
+  return transform.metadata()
+  .then(metadata => {
+    diff = metaDiff(metadata, query)
+    const shrunk = shrinkToFit([metadata.height, metadata.width], [diff.height, diff.width])
+    if (shrunk) {
+      diff = {
+        ...diff,
+        height: shrunk[0],
+        width: shrunk[1]
+      }
+    } else {
+      delete diff.height;
+      delete diff.width;
+    }
+    if (diff.format) {
+      transform = transform.toFormat(diff.format)
+    } else {
+      diff.format = metadata.format
+    }
+  })
+  .then(() => {
+    if (diff.height || diff.width) {
+      transform = transform.resize(diff.width, diff.height)
+    }
+
+    diff.src = src
+
+    const outFile = path.join(options.cachePath, hash(diff) + '.' + diff.format)
+
+    if (fs.existsSync(outFile)) {
+      const stat = fs.statSync(outFile)
+      const mtime = moment(stat.mtime)
+      const age = moment().diff(mtime, 'seconds')
+      if (age < options.maxAge) {
+        console.log('Cached: ', outFile)
+        return outFile
+      } else {
+        console.log('Purged: ', outFile)
+        fs.unlinkSync(outFile)
+      }
+    }
+
+    console.log('Generated: ', outFile)
+    return transform.toFile(outFile).then(() => outFile)
+  })
+
+}
+
+const metaDiff = (a, b) => {
+  return Object.keys(b).reduce((acc, key) => {
+    if (a.hasOwnProperty(key)) {
+      if (a[key] !== b[key]) {
+        return { ...acc, [key]: b[key] }
+      }
+    }
+    return acc
+  }, {})
+}
--- src/server/ExpressImageMiddleware/index.js
+++ src/server/ExpressImageMiddleware/index.js
@@ -1,10 +1,7 @@
 import path from 'path'
 import fs from 'fs'
 import os from 'os'
-import moment from 'moment'
-import sharp from 'sharp'
-import hash from 'object-hash'
-import shrinkToFit from './shrinkToFit'
+import imageResizeAndCache from './imageResizeAndCache'
 
 export default (
   options = {}
@@ -22,8 +19,6 @@ export default (
 
   options = Object.assign({}, defaults, options)
 
-  console.log(options)
-
   if (!fs.existsSync(options.cachePath)) {
     fs.mkdirSync(options.cachePath, { recursive: true })
   }
@@ -31,10 +26,6 @@ export default (
   return (req, res, next) => {
     const { path: file, params, method } = req
     let { query } = req
-    console.log(method)
-    console.log(file)
-    console.log(params)
-    console.log(query)
 
     query = {
       ...query,
@@ -43,69 +34,11 @@ export default (
     }
     const src = path.join(options.basePath, file)
 
-    let diff={}
-    let transform = sharp(src)
-    transform.metadata()
-    .then(metadata => {
-      diff = metaDiff(metadata, query)
-      const shrunk = shrinkToFit([metadata.height, metadata.width], [diff.height, diff.width])
-      if (shrunk) {
-        diff = {
-          ...diff,
-          height: shrunk[0],
-          width: shrunk[1]
-        }
-      } else {
-        delete diff.height;
-        delete diff.width;
-      }
-      if (diff.format) {
-        transform = transform.toFormat(diff.format)
-      } else {
-        diff.format = metadata.format
-      }
+    imageResizeAndCache(src, query, options)
+    .then(cachedFile => {
+      // res.type(path.extname(cachedFile))
+      fs.createReadStream(cachedFile).pipe(res)
     })
-    .then(() => {
-      if (diff.height || diff.width) {
-        transform = transform.resize(diff.width, diff.height)
-      }
 
-      diff.src = src
-
-      console.log('diff', diff)
-
-      const outFile = path.join(options.cachePath, hash(diff) + '.' + diff.format)
-
-      if (fs.existsSync(outFile)) {
-        const stat = fs.statSync(outFile)
-        console.log(stat.mtime)
-        const mtime = moment(stat.mtime)
-        const age = moment().diff(mtime, 'seconds')
-        if (age < options.maxAge) {
-          console.log('Cached: ', outFile)
-          return { outFile }
-        } else {
-          console.log('Purged: ', outFile)
-          fs.unlinkSync(outFile)
-        }
-      }
-      console.log('Generated: ', outFile)
-      return transform.toFile(outFile).then(info => ({ outFile, info }))
-    })
-    .then(({ outFile, info }) => {
-      // res.type('image/jpg')
-      fs.createReadStream(outFile).pipe(res)
-    })
   }
-}
-
-const metaDiff = (a, b) => {
-  return Object.keys(b).reduce((acc, key) => {
-    if (a.hasOwnProperty(key)) {
-      if (a[key] !== b[key]) {
-        return { ...acc, [key]: b[key] }
-      }
-    }
-    return acc
-  }, {})
 }
