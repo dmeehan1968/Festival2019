14304bd0985677af9db78d7a4f7a70423c4a3907
Dave Meehan <dave_meehan@replicated.co.uk>
2019-04-15 17:22:01 +0100

    Refactor Image

--- src/app/components/Image/index.js
+++ src/app/components/Image/index.js
@@ -1,95 +1,85 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useReducer } from 'react'
 import Waypoint from 'react-waypoint'
 import styled from 'styled-components'
 
-const PlaceholderWrapper = styled.div`
-  background-color: red;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: top left;
-  width: 100%;
-  height: auto;
-  max-width: 100%;
-`
-
-const SvgWrapper = styled.svg`
-  width: 100%;
-  height: auto;
-  max-width: 100%;
-`
-
-const ImgWrapper = styled.img`
-  width: 100%;
-  height: auto;
-  max-width: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-`
-const PlaceholderNoRef = ({
-  height,
-  width,
-  getSrc,
-  innerRef,
-  lqip,
+export default ({
+  src,
   ...props,
 }) => {
-  let renderedWidth, renderedHeight
-  const svgRef = useRef(null)
-  if (svgRef.current) {
-    const rect = svgRef.current.getBoundingClientRect()
-    renderedWidth = Math.round(rect.width)
-    renderedHeight = Math.round(rect.height)
+  const reducer = (state, { type }) => {
+    console.log(type)
+    switch (type) {
+      case 'loading': return { loading: true, loaded: false }
+      case 'loaded': return { loading: false, loaded: true }
+      default: throw new Error()
+    }
   }
+  const [ state, dispatch ] = useReducer(reducer, {})
+  const sizeRef = useRef(null)
+  let imgWidth
+  const devicePixelRatio = typeof window !== 'undefined' && window && window.devicePixelRatio || 1
 
-  const src = getSrc(renderedWidth)
+  if (sizeRef.current) {
+    const rect = sizeRef.current.getBoundingClientRect()
+    imgWidth = Math.round(rect.width)
+  }
+  
   return (
-    <PlaceholderWrapper
-      ref={innerRef}
-      style={{
-        backgroundImage: lqip && `url("${lqip}")`
-      }}
-    >
-      <SvgWrapper
-        ref={svgRef}
-        width={width}
-        height={height}
-      />
-      {src &&
-        <ImgWrapper
-          src={src}
-          {...props}
-          width={width}
-          height={height}
-        />
-      }
-    </PlaceholderWrapper>
+    <Waypoint onEnter={()=>(!state.loading && !state.loaded) && dispatch({ 'type': 'loading' })}>
+      <div
+        style={{
+          backgroundColor: 'red',
+          width: '100%',
+          height: 'auto',
+          maxWidth: '100%',
+          position: 'relative',
+          lineHeight: 0,
+        }}
+      >
+        {!state.loaded &&
+          <svg
+            ref={sizeRef}
+            height={props.height}
+            width={props.width}
+            style={{
+              width: '100%',
+              height: 'auto',
+              maxWidth: '100%',
+            }}
+          />
+        }
+        {(state.loading || state.loaded) &&
+          <img
+            {...props}
+            src={typeof src === 'function' ? src(Math.round(imgWidth*devicePixelRatio)) : src}
+            srcSet=""
+            sizes=""
+            onLoad={e=>(!state.loaded && e.target.complete) && dispatch({type:'loaded'})}
+            style={{
+              width: '100%',
+              height: 'auto',
+              maxWidth: '100%',
+              opacity: 0,
+              transition: 'opacity 2s',
+              ...(state.loaded && {
+                opacity: 1,
+              } || {
+                position: 'absolute',
+                top: 0,
+                left: 0,
+              }),
+            }}
+          />
+        }
+      </div>
+    </Waypoint>
   )
 }
 
-const Placeholder = React.forwardRef((props, ref) => (
-  <PlaceholderNoRef ref={ref} {...props} />
-))
-
-export default ({
-  src,
-  height,
-  width,
-  lqip,
-  ...props,
-}) => {
-  const [visible, setVisible] = useState(false)
-  return (
-    <Waypoint
-      onEnter={() => {!visible && setVisible(true)}}
-    >
-      <Placeholder
-        height={height}
-        width={width}
-        getSrc={width => visible && src+`?width=${width*(window && window.devicePixelRatio || 1)}`}
-        lqip={lqip}
-        {...props}
-      />
-      </Waypoint>
-  )
+export const createSrcSet = (maxWidth, breakpoints = [ 320, 530, 950, 1130 ], densities = [ 1, 2, 3 ]) => {
+  return [...breakpoints.filter(bp=>bp <= maxWidth).reduce((acc, bp) => {
+    densities.forEach(d => acc.add(d * bp))
+    return acc
+  }, new Set([maxWidth]))]
+  .sort((a,b)=>a-b)
 }
