5429993d624f4557cc16aa9742480a1b18c00b26
Dave Meehan <dave_meehan@replicated.co.uk>
2019-04-15 10:36:54 +0100

    Calculate height/width for images

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,6 +3,8 @@ import ReactDOMServer from 'react-dom/server'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import { StaticRouter } from 'react-router-dom'
+import sharp from 'sharp'
+import path from 'path'
 
 import App from 'app/components/App'
 import reducers, { setEvent, setEvents, setDates } from 'app/ducks'
@@ -10,9 +12,10 @@ import Html from 'app/helpers/Html'
 
 export function get(req, res, next) {
   const db = req.app.get('db')
+  const imagePath = req.app.get('imagePath')
   const routerContext = {}
 
-  return fetchData(db)
+  return fetchData(db, imagePath)
     .then(getStore)
     .then(renderPage.bind(null, req.url, routerContext, res.locals))
     .then(page => res.send(page))
@@ -23,9 +26,36 @@ const sequelizeInstanceToJSON = instance => instance.toJSON()
 
 const sequelizeArrayToJSON = arr => arr.map(sequelizeInstanceToJSON)
 
-const fetchData = (db) => {
+const checkImageDimensions = (imagePath, events) => {
+
+  return Promise.all(
+    events.map(event => {
+
+      return Promise.all(event.images.map(image => {
+        if (!image.width || !image.height) {
+          const file = path.join(imagePath, '..', image.filename)
+          return sharp(file).metadata().then(metadata => {
+            image.height = metadata.height
+            image.width = metadata.width
+            // console.log('dimensions for', image.filename, image.width, image.height)
+            // TODO: save image (move this routine to before sequelizeArrayToJSON)
+            return image
+          }).catch(err => console.log(err))
+        }
+      }))
+      .then(images => {
+        event.images = images
+        event.preferred_image = images.find(image => image.id == event.preferred_image.id)
+        return event
+      })
+    })
+  )
+
+}
+
+const fetchData = (db, imagePath) => {
   const requests = {
-    events: db.models.events.findAll().then(sequelizeArrayToJSON),
+    events: db.models.events.findAll().then(sequelizeArrayToJSON).then(checkImageDimensions.bind(null, imagePath)),
     dates: db.models.dates.findAll().then(sequelizeArrayToJSON),
     venues: db.models.venues.scope('venuesmap').findAll().then(sequelizeArrayToJSON),
     disciplines: db.models.tags.scope('disciplines').findAll().then(sequelizeArrayToJSON),
