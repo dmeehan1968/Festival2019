f9305660be28521430ca91702ecde28d9e3b8f5b
Dave Meehan <dave_meehan@replicated.co.uk>
2019-04-25 18:37:22 +0100

    Slide transition

--- src/app/components/Carousel/ControlledCarousel.js
+++ src/app/components/Carousel/ControlledCarousel.js
@@ -8,6 +8,26 @@ const Wrapper = styled.div.withConfig({displayName: 'Wrapper'})`
   justify-items: center;
   align-items: center;
   overflow: hidden;
+  position: relative;
+  line-height: 0;
+`
+
+const Slide = styled.div.withConfig({displayName: 'Slide'})`
+
+  position: absolute;
+  transition: transform ${p=>p.speed || '1s'};
+
+  &.active {
+    transform: translateX(0);
+  }
+
+  &.previous {
+    transform: translateX(-${p=>p.offset || 1000}px);
+  }
+
+  &.next {
+    transform: translateX(${p=>p.offset || 1000}px);
+  }
 `
 
 export const ControlledCarousel = ({
@@ -15,6 +35,7 @@ export const ControlledCarousel = ({
   width = 0,
   activeIndex = 0,
   children,
+  speed = '1s',
   ...props,
 }) => {
   activeIndex = Math.min(Math.max(0, activeIndex), React.Children.count(children)-1)
@@ -28,15 +49,23 @@ export const ControlledCarousel = ({
       {React.Children.count(children) < 1 &&
         <div>There is no content to display</div>
       }
-      {activeChild &&
-        React.cloneElement(
-          activeChild,
-          {
-            ['data-carousel-height']: height,
-            ['data-carousel-width']: width,
-          }
+      {React.Children.map(children, (child, key)=> {
+        return (
+          <Slide
+            className={key < activeIndex ? 'previous' : key > activeIndex ? 'next' : 'active'}
+            offset={width}
+            speed={speed}
+          >
+            {React.cloneElement(
+              child,
+              {
+                ['data-carousel-height']: height,
+                ['data-carousel-width']: width,
+              }
+            )}
+          </Slide>
         )
-      }
+      })}
       <noscript>
         {children}
       </noscript>
