0a44b548ce835fb06fd6d45be61987aae4f0f2a4
Dave Meehan <dave_meehan@replicated.co.uk>
2019-04-30 17:08:22 +0100

    Extract PORT and DB_* settings to .env

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import path from 'path'
+import assert from 'assert'
 import database from './database'
 import manifest from './manifest'
 import paths from '../../config/paths'
@@ -58,7 +59,19 @@ function log(context) {
   console.log(`Listening on port ${context.port}`)
 }
 
-boot({ port: 3000, dbhost: 'localhost', dbschema: '10p_festival2017', dbuser: 'root', dbpass: 'root' })
+assert(process.env.PORT, 'PORT not defined in .env')
+assert(process.env.DB_HOST, 'DB_HOST not defined in .env')
+assert(process.env.DB_SCHEMA, 'DB_SCHEMA not defined in .env')
+assert(process.env.DB_USER, 'DB_USER not defined in .env')
+assert(process.env.DB_PASS, 'DB_PASS not defined in .env')
+
+boot({
+  port: process.env.PORT, 
+  dbhost: process.env.DB_HOST,
+  dbschema: process.env.DB_SCHEMA,
+  dbuser: process.env.DB_USER,
+  dbpass: process.env.DB_PASS,
+})
   .then(listen)
   .then(log)
   .catch(e => console.error(`Error: ${e.stack}`))
