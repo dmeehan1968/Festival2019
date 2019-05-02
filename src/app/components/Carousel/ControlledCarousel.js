41132aefd8f1d9d6e3310d824f68689afd276c9b
Dave Meehan <dave_meehan@replicated.co.uk>
2019-05-02 16:25:07 +0100

    Refactor offsetX calcs

--- src/app/components/Carousel/ControlledCarousel.js
+++ src/app/components/Carousel/ControlledCarousel.js
@@ -15,7 +15,7 @@ const Wrapper = styled.div.withConfig({displayName: 'Wrapper'})`
 const Slide = styled.div.withConfig({displayName: 'Slide'})`
 
   position: absolute;
-  transition: transform ${p=>p.speed || '1s'};
+  transition: transform ${p=>`${p.speed}ms` || '1000ms'};
   transform: translateX(${p=>p.offsetX}px);
 
 `
@@ -25,7 +25,7 @@ export const ControlledCarousel = ({
   width = 0,
   activeIndex = 0,
   children,
-  speed = '1s',
+  speed = 1000,
   columnGap = 20,
   isClient = true,
   ...props,
@@ -35,7 +35,6 @@ export const ControlledCarousel = ({
     const imageRatio = child.props.width / child.props.height
     return Math.round(height * imageRatio)
   })
-  console.log(widths);
   return (
     <Wrapper
       {...props}
@@ -47,29 +46,16 @@ export const ControlledCarousel = ({
       }
       {isClient && React.Children.map(children, (child, index)=> {
         const offset = index - activeIndex
-        // const offsetX = (computedWidth+columnGap) * offset
-        /*
-         400     X =
-         300 <-- X = 0
-         200     X =
-        */
-        let offsetX = 0
 
-        if (offset > 0) {
-          const intermediates = widths.slice(Math.min(activeIndex+1, index), Math.max(activeIndex+1, index))
-          console.log(index, activeIndex, intermediates);
-          offsetX = intermediates.reduce((acc, width) => acc+width, 0)
-          offsetX += (widths[index] - ((widths[index] - widths[activeIndex])/2)) + (columnGap * offset)
-        }
-
-        if (offset < 0) {
-          const intermediates = widths.slice(Math.min(activeIndex, index+1), Math.max(activeIndex, index+1))
-          console.log(index, activeIndex, intermediates);
-          offsetX = intermediates.reduce((acc, width) => acc+width, 0)
-          offsetX += (widths[index] - ((widths[index] - widths[activeIndex])/2)) + (columnGap * Math.abs(offset))
-          offsetX *= -1
-        }
-
+        const boostActiveIndex = offset > 0 ? 1 : 0
+        const boostIndex = offset < 0 ? 1 : 0
+        const lowerBound = Math.min(activeIndex + boostActiveIndex, index + boostIndex)
+        const upperBound = Math.max(activeIndex + boostActiveIndex, index + boostIndex)
+        const intermediates = widths.slice(lowerBound, upperBound)
+        const sum = (acc, value) => acc+value
+        let offsetX = intermediates.reduce(sum, 0)
+        offsetX += (widths[index] - ((widths[index] - widths[activeIndex])/2)) + (columnGap * Math.abs(offset))
+        offsetX *= Math.sign(offset)
 
         return (
           <Slide
