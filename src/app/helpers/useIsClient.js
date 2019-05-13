0bcb31bb4a189ba4d5d7c6f3b1b348a565940bf2
Dave Meehan <dave_meehan@replicated.co.uk>
2019-05-13 11:34:08 +0200

    Disable client in test mode

--- src/app/helpers/useIsClient.js
+++ src/app/helpers/useIsClient.js
@@ -2,6 +2,6 @@ import { useState, useEffect } from 'react'
 
 export default () => {
   const [ isClient, setIsClient ] = useState(false)
-  useEffect(() => { setIsClient(true) })
+  useEffect(() => { setIsClient(process.env.NODE_ENV !== 'test' ? true : false) })
   return isClient
 }
