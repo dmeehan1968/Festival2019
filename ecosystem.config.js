09a07fd5ef337251a319a6eddb46c7983e92288c
Dave Meehan <dave_meehan@replicated.co.uk>
2019-05-14 01:37:11 -0700

    Create PM2 ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps : [{
+    name: 'Festival2019',
+    script: './build/server/server.bundle.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && npm run build && npx pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
