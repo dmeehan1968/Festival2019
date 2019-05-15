8cca671458ea5eabb5bc5665ee925542dd9dfef1
Dave Meehan <dave_meehan@replicated.co.uk>
2019-05-15 00:45:11 -0700

    Fix pm2_boot.sh

--- pm2_boot.sh
+++ pm2_boot.sh
@@ -1,1 +1,9 @@
-/home/replicated/.nvm/versions/node/v11.9.0/bin/node -- /home/replicated/beta.10parishesfestival.org.uk/node_modules/pm2/bin/pm2 start /home/replicated/beta.10parishesfestival.org.uk/ecosystem.config.js --env production
+#!/bin/sh
+
+export NODE_PATH=/home/replicated/.nvm/versions/node/v11.9.0/bin
+export PATH=$NODE_PATH:$PATH
+export APPDIR=/home/replicated/beta.10parishesfestival.org.uk
+
+cd $APPDIR
+
+npx pm2 start --env production
