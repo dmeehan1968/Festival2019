6515c836e4e03d209ecf0963150be3168da125af
Dave Meehan <dave_meehan@replicated.co.uk>
2019-05-16 15:02:57 +0200

    Remove react-map-gl code

--- src/app/components/Map/index.js
+++ src/app/components/Map/index.js
@@ -1,16 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import ReactMap, { Popup, NavigationControl } from 'react-map-gl'
-import { NavigationControlWrapper } from './styles'
-import WebMercatorViewport from 'viewport-mercator-project'
 import useIsClient from 'app/helpers/useIsClient'
 import styled from 'styled-components'
 
-const WarningMessage = styled.div`
-  display: grid;
-  height: 100%;
-  justify-content: center;
-  align-content: center;
-`
 
 export default ({
   children,
@@ -38,64 +29,11 @@ export default ({
     zoom: zoom,
   })
 
-  useEffect(() => {
-
-    if (!viewport.height || !viewport.width) {
-      return
-    }
-
-    const expand=0.005
-    const { latitude, longitude, zoom } = new WebMercatorViewport({ height: viewport.height, width: viewport.width })
-      .fitBounds([ [ bounds.minLng-expand, bounds.minLat-expand ], [ bounds.maxLng+expand, bounds.maxLat+expand ] ], {padding: 100})
-
-    setViewport({
-      ...viewport,
-      latitude,
-      longitude,
-      zoom,
-    })
-
-  }, [ bounds, viewport.height, viewport.width ])
-
-  if (isClient && ReactMap.supported()) {
     return (
-      <ReactMap
-        {...viewport}
-        {...otherProps}
-        height={height}
-        width={width}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        onViewportChange={viewport => setViewport(viewport)}
-        onClick={() => setPopup(null)}
-      >
         {children}
-
-        {popup &&
-          <Popup
-            latitude={popup.latitude}
-            longitude={popup.longitude}
-            closeOnClick={false}
-            closeButton={false}
-          >
-            {popup.content}
-          </Popup>
-        }
-
-        <NavigationControlWrapper>
-          <NavigationControl onViewportChange={viewport => setViewport(viewport)}/>
-        </NavigationControlWrapper>
-
-      </ReactMap>
     )
   } else {
     return (
-      <WarningMessage>
-        {ReactMap.supported() &&
-          <span suppressHydrationWarning={true}>Loading Map...</span>
-          ||
-          <span>Maps are not supported on your browser</span>
-        }
-      </WarningMessage>
     )
   }
 }
