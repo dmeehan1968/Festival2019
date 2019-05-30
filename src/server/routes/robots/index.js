e5c089f7643279fd6d5a3db099fe0ae44bfc9398
Dave Meehan <dave_meehan@replicated.co.uk>
2019-05-30 10:13:10 +0200

    Don't allow robots to access /map and /favourites

--- src/server/routes/robots/index.js
+++ src/server/routes/robots/index.js
@@ -3,7 +3,8 @@ export const get = (req, res) => {
   res.send(
 `Sitemap: # All robots allowed
 User-agent: *
-Disallow:
+Disallow: /map
+Disallow: /favouites
 
 # Sitemap files
 Sitemap: ${req.protocol || 'http'}://${req.headers.host}/sitemap.xml
