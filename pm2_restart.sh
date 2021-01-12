58c19ff34050274549d0c068f1a531716a78a37a
Dave Meehan <dave_meehan@replicated.co.uk>
2021-01-12 02:49:16 -0800

    Create pm2_restart script

---
+++ pm2_restart.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export NODE_PATH=/home/replicated/.nvm/versions/node/v11.9.0/bin
+export PATH=$NODE_PATH:$PATH
+export APPDIR=/home/replicated/2019.10parishesfestival.org.uk
+
+cd $APPDIR
+
+npx pm2 restart all
